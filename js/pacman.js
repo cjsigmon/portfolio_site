4dba15cd7514e4c4e0df08d751e0c8b9f92e1f80
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-09-14 22:51:24 -0400

    added collision detection for canvas walls

--- js/pacman.js
+++ js/pacman.js
@@ -7,8 +7,43 @@ const player = {
     y: 220,
     width: 40,
     height:40,
-    speed: 2,
+    speedX: 2,
+    speedY: 3
 };
-
-
 ctx.drawImage(pacMan, player.x, player.y, player.width, player.height);
+
+
+
+
+
+function animate(timestamp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(pacMan, player.x, player.y, player.width, player.height);
+    
+    
+    player.x += player.speedX;
+    player.y += player.speedY;
+
+    if (player.x >= canvas.width - player.width 
+        || player.x <= 0) {
+        player.speedX = -player.speedX;
+    }
+
+    if (player.y >= canvas.height - player.height 
+        || player.y <= 0) {
+        player.speedY = -player.speedY;
+    }
+    
+
+
+
+
+
+    // Call requestAnimationFrame to schedule the next frame
+    requestAnimationFrame(animate);
+  }
+  
+  // Start the animation loop
+  requestAnimationFrame(animate);
+  
+
