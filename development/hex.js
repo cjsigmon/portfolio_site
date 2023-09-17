de28224f9bb79a6f1afdc1d0409ab19aaf2788ed
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-09-17 05:39:00 -0400

    tracking which triangle clicked

--- development/hex.js
+++ development/hex.js
@@ -4,7 +4,12 @@ const svgHeight = myShape.getBoundingClientRect().height;
 const middleX = myShape.getBoundingClientRect().width/2;
 const svgWidth = myShape.getBoundingClientRect().width;
 let counter = 0;
+let rotationCount = 0.25;
 let faces = [];
+
+// tA.addEventListener("click", function() {
+//     alert("Button clicked!");
+//   });
 
 
 class Triangle {
@@ -21,6 +26,9 @@ class Triangle {
     }
 
     rotate() {
+        if (rotationCount === 8) {
+            rotationCount = 0;
+        }
         console.log(counter);
         if (counter === 0) {
             if (this.order === 1) {
@@ -46,6 +54,8 @@ class Triangle {
 
         
         if (counter === 10) {
+            rotationCount += 0.25;
+            
             if (this.order===1) {
                 this.order++;
             } else {
@@ -81,21 +91,11 @@ class Triangle {
             } else {
                 this.order--;
             }
+            rotationCount += 0.25;
             counter = 0;
         }
-
-
-
-
-
         this.redraw();
     }
-
-
-
-
-
-
 
       // Function to create a polygon string for the triangle's points
   getPolygonString() {
@@ -106,6 +106,15 @@ class Triangle {
   drawInSVG(svgElement) {
     const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
     polygon.id = this.id;
+    polygon.addEventListener("click", function() {
+        if (rotationCount < 1 || rotationCount >= 5) {
+            alert("Triangle B clicked!");
+
+        } else {
+            alert("Triangle C clicked!");
+
+        }
+      });
     polygon.setAttribute("points", this.getPolygonString());
     polygon.setAttribute("fill", this.fillColor); // Set the fill color here
     svgElement.appendChild(polygon);
@@ -126,7 +135,7 @@ function rotateXTimes(x) {
 }
 
 function rotateForever() {
-    var intervalID = setInterval(rotate, 50);
+    var intervalID = setInterval(rotate, 100);
 
 }
 
@@ -144,7 +153,7 @@ function rotate() {
   
   // Example usage:
   const triangleC = new Triangle(0, svgHeight - 50, middleX, 0, svgWidth, svgHeight -50, "transparent", "triangleC", 3);
-  const triangleA = new Triangle(0, svgHeight - 50, middleX, 0, middleX, svgHeight, "darkgreen", "triangleA", 2);
+  const triangleA = new Triangle(0, svgHeight - 50, middleX, 0, middleX, svgHeight, "white", "triangleA", 2);
   const triangleB = new Triangle(middleX, svgHeight, middleX, 0, svgWidth, svgHeight -50, "green", "triangleB", 1);
 
   const triangleF = new Triangle(0, svgHeight - 50, middleX, 0, svgWidth, svgHeight -50, "transparent", "triangleF", 3);
